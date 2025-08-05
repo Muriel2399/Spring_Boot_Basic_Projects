fd22f00f2d3b8d14a418098e22ccbbb7bab38eb5
Muriel2399 <murielspinto@gmail.com>
2025-08-05 18:52:18 +0530

    Add files via upload

---
+++ example4/src/main/java/com/eazybytes/config/Demo.java
@@ -0,0 +1,20 @@
+package com.eazybytes.config;
+
+public class Demo {
+    public static void main ( String[] args){
+        System.out.println("Hello World");
+    }
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static int subtract(int a, int b){
+        return a-b;
+    }
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+    public static int divide(int a, int b){
+        return a/b;
+    }
+
+}
